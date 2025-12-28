a2854aed8265e16753f1ad330d6bf82a243774bf
Mia Darvasi <miadarv@gmail.com>
2025-12-28 17:54:28 +0200

    debug stay list export

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ export default {
   dbURL: process.env.MONGO_URL || 'mongodb+srv://miaginattamar_db_user:eHC5XzWHWvUZjyQ9@dogs.44ddgbu.mongodb.net/?retryWrites=true&w=majority&appName=Dogs',
   dbName: process.env.DB_NAME || 'dogs_db',
 
-  stayExportKey: process.env.STAY_EXPORT_KEY
+  stayExportKey: 'staysync_8f3KJ29dLqW91xPz'
 }
